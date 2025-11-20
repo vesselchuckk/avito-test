6c270892e6dbbc23a48586a848a0af86c5c09fbd
astroniumm <tim.grig.00@gmail.com>
2025-11-20 12:50:35 +0600

    added controller

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger, Post } from '@nestjs/common';
+import { PuppeteerService } from 'src/puppeteer/puppeteer.service';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+	private logger = new Logger('AdminController');
+	
+	constructor(private readonly puppeteer: PuppeteerService) {	}
+
+	@Post('start')
+	async start() {
+		await this.puppeteer.start();
+		return { status: 'puppeteer monitoring started' };
+		return { ok: true }
+	}
+	@Post('stop')
+	async stop() {
+		await this.puppeteer.stop();
+		return { status: 'puppeteer monitoring stopped' };
+		return { ok: true }	
+	}
+}
